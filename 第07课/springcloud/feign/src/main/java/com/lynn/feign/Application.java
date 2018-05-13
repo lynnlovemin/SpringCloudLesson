3ac8ada1113ca912c557e1ac57937e8e50e5377c
lynnlovemin <331509758@qq.com>
2018-05-13 10:35:33 +0800

    增加第7章代码

---
+++ 第07课/springcloud/feign/src/main/java/com/lynn/feign/Application.java
@@ -0,0 +1,18 @@
+package com.lynn.feign;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
