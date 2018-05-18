363a4734551e3b81d1eb879c800e9e92e099caa4
lynnlovemin <331509758@qq.com>
2018-05-18 09:40:17 +0800

    新增第09课

---
+++ 第09课/springcloud/feign/src/main/java/com/lynn/feign/ApiController.java
@@ -0,0 +1,17 @@
+package com.lynn.feign;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiController {
+
+    @Autowired
+    private ApiService apiService;
+
+    @RequestMapping("index")
+    public String index(){
+        return apiService.index();
+    }
+}
