4042c60c893a8f2ab3bfdbaed82aeb58555ba8dd
lynnlovemin <331509758@qq.com>
2018-05-19 16:36:29 +0800

    修改配置文件

--- 第09课/springcloud/eurekaclient/src/main/java/com/lynn/HelloController.java
+++ 第09课/springcloud/eurekaclient/src/main/java/com/lynn/HelloController.java
@@ -1,10 +1,12 @@
 package com.lynn;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class HelloController {
 
     @Value("${server.port}")
