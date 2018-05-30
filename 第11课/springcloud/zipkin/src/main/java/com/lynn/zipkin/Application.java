ac2e4a532dc721555b008c4cb8b54fd012a2d7dc
lynnlovemin <331509758@qq.com>
2018-05-30 14:51:48 +0800

    修改配置

--- 第11课/springcloud/zipkin/src/main/java/com/lynn/zipkin/Application.java
+++ 第11课/springcloud/zipkin/src/main/java/com/lynn/zipkin/Application.java
@@ -2,10 +2,12 @@ package com.lynn.zipkin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
 import zipkin.server.EnableZipkinServer;
 
 @SpringBootApplication
-@EnableZipkinServer
+//@EnableZipkinServer
+@EnableZipkinStreamServer
 public class Application {
 
     public static void main(String[] args) {
