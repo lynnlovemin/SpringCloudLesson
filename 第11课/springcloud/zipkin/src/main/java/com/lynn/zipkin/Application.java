c8de3280dacece364c58430145ed4649b1ea5d53
lynnlovemin <331509758@qq.com>
2018-05-30 19:27:59 +0800

    增加11课

--- 第11课/springcloud/zipkin/src/main/java/com/lynn/zipkin/Application.java
+++ 第11课/springcloud/zipkin/src/main/java/com/lynn/zipkin/Application.java
@@ -3,7 +3,12 @@ package com.lynn.zipkin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 import zipkin.server.EnableZipkinServer;
+import zipkin.storage.mysql.MySQLStorage;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 //@EnableZipkinServer
@@ -12,5 +17,11 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class,args);
+    }
+
+    @Bean
+    @Primary
+    public MySQLStorage mySQLStorage(DataSource datasource) {
+        return MySQLStorage.builder().datasource(datasource).executor(Runnable::run).build();
     }
 }
